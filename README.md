a40c4c648ba8a8602877c82c3936e473716c0a2a
canbax <yusufsaidcanbaz@gmail.com>
2022-01-01 15:16:32 +0300

    start the project

---
+++ README.md
@@ -0,0 +1,37 @@
+# Kur'an'dan Mesaj
+
+Rastgele bir Kur'an mealinden, rastgele bir pasaj paylaşır.
+
+  <h2> Açıklama </h2>
+  <p>
+    <a target="_blank" href="https://twitter.com/kurandan_mesaj" class="link-primary">
+      <img src="doc/twitter.svg" style="background: white; border-radius: 50%;"> Twitter'da
+    </a>
+    takip edebilirsiniz. Her gün sadece 1 tweet atar.
+  </p>
+  <p>
+    <a target="_blank" href="https://t.me/kurandanmesaj" class="link-primary">
+      <img src="doc/telegram.svg" style="background: white; border-radius: 50%;"> Telegram kanalına</a> katılabilirsiniz. Her gün sadece 1 mesaj atar.
+  </p>
+  <p>
+    <a target="_blank" href="https://t.me/kurandan_bot" class="link-primary">
+    <img src="doc/robot.svg" style="background: white; border-radius: 50%;"> Telegram botunu </a> rastgele pasajlar almak için kullanabilirsiniz.
+  </p>
+
+  <h2> Hakkında </h2>
+  <p>
+    Ücretsiz, reklamsız ve izleyicisizdir (tracker ya da analytics scriptleri kullanmaz).
+  </p>
+
+  <p>
+    <a target="_blank" href="https://github.com/ziegfiroyt/acikkuran-api" class="link-info">Açık Kuran API</a> kullanır.
+    <a target="_blank" href="https://www.postman.com/canbax/workspace/ak-kuran/overview"
+      class="link-info">Postman'de</a> Açık Kuran API
+    incelenebilir.
+  </p>
+  <p>
+    Her gün Türkiye saati ile 19'da bir private <a target="_blank" href="https://gitlab.com/canbax/daily-webhooker"
+      class="link-info">GitLab</a> projesinde tanımlı bir pipeline schedule sayesinde heroku'daki ücretsiz sunucuya
+    (burası oluyor) HTTP
+    isteği gönderir. Bu istek gelince twitter ve telegramda pasaj paylaşılır.
+  </p>
---
+++ server.js
@@ -0,0 +1,128 @@
+// require("./env");
+const express = require("express");
+const app = express();
+const got = require("got");
+const bodyParser = require("body-parser");
+
+// allow every browser to get response from this server, this MUST BE AT THE TOP
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+app.use(bodyParser.json());
+
+if (!process.env.TELEGRAM_BOT_TOKEN) {
+  console.error("'TELEGRAM_BOT_TOKEN' should be an environment variable");
+  return -1;
+}
+const TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+const URL = "https://api.telegram.org/bot" + TOKEN + "/";
+const welcomeMsg =
+  "Merhaba. Ben şiir getiren bir botum :) /siir komutu ile rastgele getirebilirsiniz.";
+
+function errResponseFn(err, res) {
+  console.log(err);
+  res.write("Error: ", JSON.stringify(err));
+  res.end();
+}
+
+// get telegram updates using webhook
+app.post("/tupdate", async (req, res) => {
+  try {
+    console.log("telegram update come with message: ", req.body.message.text);
+    await processInput(req.body.message.text, req.body.message.chat.id);
+    res.write("received telegram update: ", req.body);
+    res.end();
+  } catch (err) {
+    console.log("request body: ", typeof req.body, req.body);
+    errResponseFn(err, res);
+  }
+});
+
+// respond to gitlab request
+app.post("/daily", async (req, res) => {
+  try {
+    console.log("daily post: ", typeof req.body, req.body);
+    if (req.body.pwd != TOKEN) {
+      res.write("need password!");
+      res.end();
+    } else {
+      let txt = await getRandomPoem();
+      await sendTelegramMsg(txt, "@her_gun_siir");
+      res.write("received daily post from gitlab");
+      res.end();
+    }
+  } catch (err) {
+    console.log("request body: ", typeof req.body, req.body);
+    errResponseFn(err, res);
+  }
+});
+
+async function processInput(txt, chatId) {
+  if (txt == "/siir") {
+    let s = await getRandomPoem();
+    await sendTelegramMsg(s, chatId);
+  } else if (txt == "/start") {
+    await sendTelegramMsg(welcomeMsg, chatId);
+  }
+}
+
+async function sendTelegramMsg(msg, chatId) {
+  await got.post(URL + "sendMessage", {
+    json: { chat_id: chatId, text: msg },
+  });
+}
+
+async function getRandomPoem() {
+  return "";
+}
+
+async function hasWebhook() {
+  try {
+    const { body } = await got(URL + "getWebhookInfo");
+    const b = JSON.parse(body);
+    return b.result.url.length > 0;
+  } catch (err) {
+    console.log("Error: ", err);
+  }
+}
+
+async function setWebhook() {
+  const webhook = "https://her-gun-siir.herokuapp.com/tupdate";
+  try {
+    if (await hasWebhook()) {
+      return;
+    }
+    const { body } = await got(URL + "setWebhook?url=" + webhook);
+    const b = JSON.parse(body);
+    return b.result.url.length > 0;
+  } catch (err) {
+    console.log("Error: ", err);
+  }
+}
+
+async function setCommands() {
+  try {
+    const b = {
+      commands: [{ command: "siir", description: "Rastgele şiir getirir" }],
+    };
+    const { body } = await got.post(URL + "setMyCommands", { json: b });
+  } catch (err) {
+    console.log("Error: ", err);
+  }
+}
+
+async function main() {
+  app.use(express.static("public"));
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => console.log("server on " + PORT));
+
+  await setWebhook();
+  await setCommands();
+}
+
+main();
